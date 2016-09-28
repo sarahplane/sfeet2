5a191a0c07b1b6a5306245857852793d0c76c787
Sarah Plane <sarahmplane@gmail.com>
2016-09-28 09:57:19 -0600

    Products shell

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,22 @@
+class ProductsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def create
+  end
+end
