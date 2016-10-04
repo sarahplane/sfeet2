16e4adf54c84e633aef6c9587ec5df50ec745d99
Sarah Plane <sarahmplane@gmail.com>
2016-10-04 15:33:51 -0600

    New products with categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,6 +7,7 @@ class CategoriesController < ApplicationController
   end
 
   def show
+    @categories = Category.all
   end
 
   def new
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,6 @@ class Category < ActiveRecord::Base
   has_many :products
 
   validates :name, presence: true, length: { minimum: 2 }, uniqueness: true
+
+  scope :sorted, -> {order(:name)}
 end
