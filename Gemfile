fa3cafdab253e312746e683c99f996a655feeee3
Sarah Plane <sarahmplane@gmail.com>
2016-10-16 13:27:24 -0600

    SimpleCov

--- Gemfile
+++ Gemfile
@@ -52,6 +52,8 @@ group :development, :test do
   gem 'byebug'
   # Use sqlite3 as the database for Active Record
   gem 'sqlite3'
+
+  gem 'faker'
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'faker'
+
+old_products_count = Product.count
+categories = Category.all
+tags = Tag.all
+
+num_to_add = [0, (50 - Product.count)].max
+num_to_add.times do
+  product = Product.create(name: Faker::Hipster.words(2).join(' '), price: rand(4)+1, tag_list: tags.sample, category_id: categories.sample)
+end
+
+puts "Added #{num_to_add} Products to the #{old_products_count} Products that were already there."
