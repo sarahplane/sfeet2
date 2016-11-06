2e378740ab68e323eac7c79f7f730a6e356afddf
Sarah Plane <sarahmplane@gmail.com>
2016-11-06 10:51:36 -0700

    rspec refactor

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -2,72 +2,90 @@ require "rails_helper"
 
 RSpec.describe ProductsController, :type => :controller do
 
-  include Devise::Test::ControllerHelpers
-
-  let(:user){User.create(email: "user2@user.com", password: "123456", admin: false, password_confirmation: "123456", confirmed_at: Time.now)}
   let(:product1) {Product.create(name: "Product1", price: "3")}
   let(:category1){Category.create(name: "Cat1")}
 
-#use a context "for admin user"
-# and set up a before each to sign in user
 
-  describe "GET #index" do
-    it "succeeds" do
-      get :index
+  context "non-user" do
 
-      expect(response).to be_success
-    end
-  end
+    describe "GET #index" do
+      it "succeeds" do
+        get :index
 
-
-  describe "GET #new" do
-    it "redirects to sign in if you are signed out" do
-      sign_out user
-      get :new
-
-      expect(response).to redirect_to new_user_session_path
-    end
-  end
-
-  describe "POST #create" do
-    subject(:creation) {post :create, product: {name: 'Test 123', price: '1', category_id: category1.id}}
-
-    it "successfully creates a product" do
-      sign_in user
-
-      expect{creation}.to change{Product.count}.by(1)
-    end
-
-    it "raises an error if missing params" do
-      sign_in user
-      product1 = {product: {name: nil}}
-
-      expect do
-        post :create, product
-        (product1.valid?).to_not eq true
+        expect(response).to be_success
       end
-      #refactor away from expect do block!
     end
+
+    describe "GET #show" do
+      it "succeeds" do
+        get :show, id: product1
+
+        expect(response).to be_success
+      end
+    end
+
+    describe "GET #new" do
+      it "redirects to sign in if you are signed out" do
+        get :new
+
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
   end
 
-  describe "PUT #update" do
 
-    it "successfully updates changed to product" do
-      sign_in user
-      put :update, :id => product1.id, product: {price: "5"}
-      product1.reload
+  context "Admin user" do
 
-      expect(product1.price).to eq "5"
+    before (:each) do
+      @admin = User.create(email: "admin@user.com", password: "123456", admin: true, password_confirmation: "123456", confirmed_at: Time.now)
     end
+
+    describe "PUT #update" do
+      it "successfully updates changed to product" do
+        sign_in @admin
+        put :update, :id => product1.id, product: {price: "5"}
+        product1.reload
+
+        expect(product1.price).to eq "5"
+      end
+    end
+
+    describe "DELETE #destroy" do
+      it "successfully deletes an product" do
+        sign_in @admin
+        delete :destroy, :id => product1.id, method: :delete
+
+        expect(Product.count).to eq 0
+      end
+    end
+
   end
 
-  describe "DELETE #destroy" do
-    it "successfully deletes an product" do
-      sign_in user
-      delete :destroy, :id => product1.id, method: :delete
 
-      expect(Product.count).to eq 0
+  context "Non-Admin User" do
+
+    before (:each) do
+      @user = User.create(email: "user2@user.com", password: "123456", admin: false, password_confirmation: "123456", confirmed_at: Time.now)
     end
+
+    describe "POST #create" do
+      subject(:creation) {post :create, product: {name: 'Test 123', price: '1', category_id: category1.id}}
+
+      it "successfully creates a product" do
+        sign_in @user
+
+        expect{creation}.to change{Product.count}.by(1)
+      end
+
+      it "raises an error if missing params" do
+        sign_in @user
+        product1 = Product.create(name: nil)
+
+        expect(product1.errors[:name]).to include("can't be blank")
+      end
+    end
+
   end
 
 end
