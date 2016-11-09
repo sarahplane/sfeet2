025aa575a9ee2d8c1e82f78d17fbf139280d20f8
Sarah Plane <sarahmplane@gmail.com>
2016-11-09 16:12:37 -0700

    rspec bug fix

--- spec/controllers/tags_controller_spec.rb
+++ spec/controllers/tags_controller_spec.rb
@@ -4,19 +4,25 @@ RSpec.describe TagsController, :type => :controller do
 
   include Devise::Test::ControllerHelpers
 
-  before(:each) do
-    @user = User.create(email: "12345@gmail.com", password: "123456", password_confirmation: "123456")
-    @user.confirm
-    @user.save
-    sign_in @user
-  end
+  let(:user) {User.create(email: "admin@user.com", password: "123456", admin: true, password_confirmation: "123456", confirmed_at: Time.now)}
+  let(:product1) {Product.create(name: "Product1", price: "3")}
+  let(:tag) {Tag.create(name: "Tag1")}
 
   describe "DELETE #destroy" do
     it "successfully deletes a tag" do
+      sign_in user
       @tag = Tag.create(name: "Snackaroo")
       delete :destroy, :id => @tag.id
 
       expect(Tag.count).to eq(0)
+    end
+  end
+
+  describe "GET #show" do
+    it "succeeds" do
+      get :show, id: tag
+
+      expect(response).to be_success
     end
   end
 end
